7c903cf34ef94818ff988471d765065631696355
owolabi oluwasemilogo <owolabioluwasemilogo04@gmail.com>
2024-01-02 07:28:03 -0800

    p

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST.H
-#define LIST.H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 
@@ -9,7 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * 
  */
 typedef struct listint_s
 {

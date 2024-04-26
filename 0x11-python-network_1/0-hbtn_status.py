7a995f7a5f98c031c35aa6a6adfa135bf3f3eb5c
owolabi oluwasemilogo <owolabioluwasemilogo04@gmail.com>
2024-04-26 14:52:34 -0700

    com

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found in the header
+"""
+import sys
+from urllib import request
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        with request.urlopen(url) as response:
+            request_id = response.getheader("X-Request-Id")
+            print(request_id)
+
